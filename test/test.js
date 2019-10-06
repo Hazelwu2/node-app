4654512ba976c3c1a2f7a271ce6bc6bb05c54ab0
Hazel Wu <tinazx056@gmail.com>
2019-10-06 22:35:13 +0800

    Simple Node App With Test

---
+++ test/test.js
@@ -0,0 +1,9 @@
+var request = require('supertest');
+var app = require('../index.js');
+
+describe('GET /', function() {
+    it('Respond with Hello World', function(done) {
+
+        request(app).get('/').expect('Hello World', done);
+    })
+})
