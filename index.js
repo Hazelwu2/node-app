35a2c2a765622a035b5011ed374cd322e5b4ca64
Hazel Wu <tinazx056@gmail.com>
2019-10-07 00:20:47 +0800

    Hazel World

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express()
 
 app.get('/', function(req, res) {
-    res.send('Hello Hey World');
+    res.send('Hazel World');
 })
 
 app.listen(process.env.PORT || 3000)
