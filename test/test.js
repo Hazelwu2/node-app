dee834003748a3c7c2f9a4e55c7495e2fcf0c076
Hazel Wu <tinazx056@gmail.com>
2019-10-07 00:22:48 +0800

    Hazel World Test

--- test/test.js
+++ test/test.js
@@ -4,6 +4,6 @@ var app = require('../index.js');
 describe('GET /', function() {
     it('Respond with Hello World', function(done) {
 
-        request(app).get('/').expect('Hello World', done);
+        request(app).get('/').expect('Hazel World', done);
     })
 })
